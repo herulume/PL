fc17e3bfaac423f74bac03b0b7f4a59c9fd1a783
Eduardo Barbosa <herulume@protonmail.com>
2019-03-19 15:39:58 +0000

    Lint

--- article.h
+++ article.h
@@ -13,27 +13,27 @@
 using namespace std;
 
 class Article {
-  private:
-    ofstream file;
-    void htmlStart();
-    void htmlEnd();
-  public:
-    string link;
-    string id;
-    string title;
-    string authorDate;
-    vector<string> tags;
-    string category;
-    string text;
-    vector <string> abbreviations;
+    private:
+        ofstream file;
+        void htmlStart();
+        void htmlEnd();
+    public:
+        string link;
+        string id;
+        string title;
+        string authorDate;
+        vector<string> tags;
+        string category;
+        string text;
+        vector <string> abbreviations;
 
-    void setId(string id);
-    void setTitle(string title);
-    void setAuthorDate(string AuthorDate);
-    void addTag(string tag);
-    void setCategory(string category);
-    void setText(string text);
-    void addAbbreviation(string abbreviation);
-    Article(string link);
-    void printer();
+        void setId(string id);
+        void setTitle(string title);
+        void setAuthorDate(string AuthorDate);
+        void addTag(string tag);
+        void setCategory(string category);
+        void setText(string text);
+        void addAbbreviation(string abbreviation);
+        Article(string link);
+        void printer();
 } ;
