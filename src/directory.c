8cdbaa1ac45263da316e380e161ffbd6dd4f2c5f
Eduardo Barbosa <herulume@protonmail.com>
2019-03-31 14:21:54 +0000

    Add dir

---
+++ src/directory.c
@@ -0,0 +1,9 @@
+#include "headers/directory.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void createStruct(){
+    mkdir("./output", 0777);
+    mkdir("./output/articles", 0777);
+    mkdir("./output/tags", 0777);
+}
