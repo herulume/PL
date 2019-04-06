1d87302b1e942d958f0c98fc71687081f9423a8c
Eduardo Barbosa <herulume@protonmail.com>
2019-04-06 15:53:51 +0000

    Lint .c

--- Flex/src/directory.c
+++ Flex/src/directory.c
@@ -2,8 +2,10 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void createStruct(){
-    mkdir("./output", 0777);
-    mkdir("./output/articles", 0777);
-    mkdir("./output/tags", 0777);
+void
+createStruct()
+{
+        mkdir("./output", 0777);
+        mkdir("./output/articles", 0777);
+        mkdir("./output/tags", 0777);
 }
