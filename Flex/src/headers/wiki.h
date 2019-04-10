567461419ddcf46b38e7fba3753b4e634d511a19
Eduardo Jorge <herulume@protonmail.com>
2019-04-10 11:33:28 +0000

    Lint (#10)

--- Flex/src/headers/wiki.h
+++ Flex/src/headers/wiki.h
@@ -5,20 +5,21 @@
 #include "article.h"
 #include "graph.h"
 
-class Wiki {
-    public:
-        Wiki();
-        ~Wiki();
-        std::vector<Article*> articles;
-        void index();
-        void printAll();
-        void tagIndex();
-        void titleIndex();
-        void printArticles();
-        void printArticlesMD();
-        void printArticlesTex();
-        void addArticle(Article*);
-        void generateGraphs(Graph*);
+class Wiki
+{
+        public:
+                Wiki();
+                ~Wiki();
+                std::vector<Article*> articles;
+                void index();
+                void printAll();
+                void tagIndex();
+                void titleIndex();
+                void printArticles();
+                void printArticlesMD();
+                void printArticlesTex();
+                void addArticle(Article*);
+                void generateGraphs(Graph*);
 };
 
 #endif
